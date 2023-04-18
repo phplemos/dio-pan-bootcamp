e69799839bbd4ad9477034ffe8fa70efea9e33ce
phplemos <phpinheirolemos@outlook.com>
2023-04-18 15:08:32 -0300

    Continuação curso

--- aprendendo-sintaxe-java/metodos/src/SmartTv.java
+++ aprendendo-sintaxe-java/metodos/src/SmartTv.java
@@ -1,15 +1,18 @@
 public class SmartTv {
+    
     boolean ligada = false;
     int canal = 1;
     int volume = 25;
 
-    public void mudarCanal(int novoCanal){
+    public void mudarCanal(int novoCanal) {
         canal = novoCanal;
     }
-    public void aumentarCanal(){
+
+    public void aumentarCanal() {
         canal++;
     }
-    public void diminuirCanal(){
+
+    public void diminuirCanal() {
         canal--;
     }
 
@@ -17,16 +20,18 @@ public class SmartTv {
         volume++;
         System.out.println("Aumentando o volume para: " + volume);
     }
-    public void diminuirVolume(){
+
+    public void diminuirVolume() {
         volume--;
         System.out.println("Diminuindo o volume para: " + volume);
     }
-    public void ligar(){
+
+    public void ligar() {
         ligada = true;
     }
-    public void desligar(){
+
+    public void desligar() {
         ligada = false;
     }
-
 
 }
