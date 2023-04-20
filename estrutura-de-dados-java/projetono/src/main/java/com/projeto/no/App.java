0998f33827cb3b246db2554924566e2f7fa5ecd2
phplemos <phpinheirolemos@outlook.com>
2023-04-20 20:55:16 -0300

    Refatoração classe No

--- estrutura-de-dados-java/projetono/src/main/java/com/projeto/no/App.java
+++ estrutura-de-dados-java/projetono/src/main/java/com/projeto/no/App.java
@@ -2,17 +2,17 @@ package com.projeto.no;
 
 public class App {
     public static void main(String[] args) {
-        No no1 = new No("Conteudo no1");
+        No<String> no1 = new No<>("Conteudo no1");
 
-        No no2 = new No("Conteudo no2");
+        No<String> no2 = new No<>("Conteudo no2");
 
         no1.setProximoNo(no2);
 
-        No no3 = new No("Conteudo no3");
+        No<String> no3 = new No<>("Conteudo no3");
 
         no2.setProximoNo(no3);
 
-        No no4 = new No("Conteudo no4");
+        No<String> no4 = new No<>("Conteudo no4");
 
         no3.setProximoNo(no4);
 
@@ -23,7 +23,7 @@ public class App {
         System.out.println(no2);
 
         System.out.println("------------------------------------");
-        
+
         System.out.println(no1);
         System.out.println(no1.getProximoNo());
         System.out.println(no1.getProximoNo().getProximoNo());
