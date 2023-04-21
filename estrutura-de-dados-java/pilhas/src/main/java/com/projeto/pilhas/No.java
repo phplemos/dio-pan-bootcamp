6f4316ff5fb90ee360c1f266d98bf590aeb36eed
phplemos <phpinheirolemos@outlook.com>
2023-04-20 21:51:47 -0300

    Implementação Estrutura de dados Pilha em java sem uso de vetores

---
+++ estrutura-de-dados-java/pilhas/src/main/java/com/projeto/pilhas/No.java
@@ -0,0 +1,34 @@
+package com.projeto.pilhas;
+
+public class No {
+
+    private int dado;
+    private No refNo = null;
+
+    public No(int dado) {
+        this.dado = dado;
+    }
+
+    public int getDado() {
+        return dado;
+    }
+
+    public void setDado(int dado) {
+        this.dado = dado;
+    }
+
+    public No getRefNo() {
+        return refNo;
+    }
+
+    public void setRefNo(No refNo) {
+        this.refNo = refNo;
+    }
+
+    @Override
+    public String toString() {
+        // TODO Auto-generated method stub
+        return super.toString();
+    }
+
+}
