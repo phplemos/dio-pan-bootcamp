52223ec0d33e3826e084629aedf06802cb96efb1
phplemos <phpinheirolemos@outlook.com>
2023-04-24 00:46:51 -0300

    Implementação classe ListaEncadeada

--- estrutura-de-dados-java/lista_encadeada/src/main/java/com/projeto/lista_encadeada/ListaEncadeada.java
+++ estrutura-de-dados-java/lista_encadeada/src/main/java/com/projeto/lista_encadeada/ListaEncadeada.java
@@ -8,6 +8,55 @@ public class ListaEncadeada<T> {
         this.referenciaEntrada = null;
     }
 
+    private No<T> getNo(int index) {
+        validaIndice(index);
+        No<T> noAuxiliar = referenciaEntrada;
+        No<T> noRetorno = null;
+        for (int i = 0; i <= index; i++) {
+            noRetorno = noAuxiliar;
+            noAuxiliar = noAuxiliar.getProximoNo();
+        }
+        return noRetorno;
+    }
+
+    public T remove(int index) {
+        No<T> noPivor = this.getNo(index);
+
+        if (index == 0) {
+            referenciaEntrada = noPivor.getProximoNo();
+            return noPivor.getConteudo();
+        }
+        No<T> noAnterior = getNo(index - 1);
+        noAnterior.setProximoNo(noPivor.getProximoNo());
+        return noPivor.getConteudo();
+    }
+
+    public T get(int index) {
+        return getNo(index).getConteudo();
+    }
+
+    private void validaIndice(int index) {
+        if (index >= size()) {
+            int ultimoIndice = size() - 1;
+            throw new IndexOutOfBoundsException(
+                    "Nao existe conteudo no indice " + "desta lista. Esta lista so vai ate o indice " + ultimoIndice);
+        }
+    }
+
+    public void add(T conteudo) {
+        No<T> novoNo = new No<>();
+        novoNo.setConteudo(conteudo);
+        if (this.isEmpty()) {
+            referenciaEntrada = novoNo;
+            return;
+        }
+        No<T> noAuxiliar = referenciaEntrada;
+        for (int i = 0; i < this.size() - 1; i++) {
+            noAuxiliar = noAuxiliar.getProximoNo();
+        }
+        noAuxiliar.setProximoNo(novoNo);
+    }
+
     public int size() {
         int tamanhoLista = 0;
         No<T> referenciaAux = referenciaEntrada;
@@ -28,6 +77,19 @@ public class ListaEncadeada<T> {
 
     public boolean isEmpty() {
         return referenciaEntrada == null ? true : false;
+    }
+
+    @Override
+    public String toString() {
+        String strRetorno = "";
+        No<T> noAuxliar = referenciaEntrada;
+
+        for (int i = 0; i <= this.size() - 1; i++) {
+            strRetorno += "No {conteudo=" + noAuxliar.getConteudo() + "}--->";
+            noAuxliar = noAuxliar.getProximoNo();
+        }
+        strRetorno += "null";
+        return strRetorno;
     }
 
 }
