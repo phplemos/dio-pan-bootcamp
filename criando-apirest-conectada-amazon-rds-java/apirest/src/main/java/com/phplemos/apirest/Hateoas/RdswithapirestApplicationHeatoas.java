bd8dc11cff18249a69cd26d8131eb6a2a7ceb059
phplemos <phpinheirolemos@outlook.com>
2023-09-07 12:05:20 -0300

    Criando estrutura

---
+++ criando-apirest-conectada-amazon-rds-java/apirest/src/main/java/com/phplemos/apirest/Hateoas/RdswithapirestApplicationHeatoas.java
@@ -0,0 +1,14 @@
+package com.phplemos.apirest.Hateoas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RdswithapirestApplicationHeatoas {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RdswithapirestApplicationHeatoas.class, args);
+	}
+
+	}
+
